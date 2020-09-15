883c110e2c4954f214729dcf8500e3ec1af1f80c
Stefan Zdravkov <szdravkov93@gmail.com>
2020-09-15 18:00:36 +0200

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,16 +16,74 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
+        Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              )
+            ],
+          ),
         ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
+        Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceFace();
+                  },
+                  child: Text(
+                    'ROLL',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         )
       ],
     );
